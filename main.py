ecdfe0117ed76d9de15f60fb363e6893ab2894dd
Bruno Roque <bruno.roque@celfinet.com>
2017-05-24 01:13:40 +0100

    right and left click added

--- main.py
+++ main.py
@@ -5,13 +5,28 @@ class Point(Structure):
     _fields_ = [("x", c_ulong), ("y", c_ulong)]
 
 
-class Mouse(Structure):
+class PointXY:
+    x = 0
+    y = 0
 
+
+class Mouse(object):
     def QueryPosition(self):
         point = Point()
         windll.user32.GetCursorPos(byref(point))
         return point
 
+    def LeftClick(self, point):
+        windll.user32.SetCursorPos(point.x, point.y)
+        windll.user32.mouse_event(2, 0, 0, 0, 0)
+        windll.user32.mouse_event(4, 0, 0, 0, 0)
+
+    def RightClick(self, point):
+        windll.user32.SetCursorPos(point.x, point.y)
+        windll.user32.mouse_event(8, 0, 0, 0, 0)
+        windll.user32.mouse_event(16, 0, 0, 0, 0)
+
+
 mouse = Mouse()
-mp = mouse.QueryPosition()
-print(mp.x)
+mouse.RightClick(PointXY())
+print(mouse)
